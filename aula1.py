dd52c734d2953dd7c502836d268aa63e639a341d
Breno <brenolimanf@hotmail.com>
2025-10-18 18:47:29 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,10 @@
+nome = 'Breno Lima'
+tamanho_nome = len(nome)
+nova_string = '';
+
+for i in nome:
+    nova_string += i + '*'
+else:
+    nova_string = nova_string[:-1]  # Remove o último '*'
+
+print(nova_string)
