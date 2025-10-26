f1b4dab0d99b29934dbef3443dc5c2221826d27a
Breno <brenolimanf@hotmail.com>
2025-10-26 18:59:17 -0300

    concluído estudo POO

---
+++ POO/estudo_dataclasses.py
@@ -0,0 +1,34 @@
+# dataclasses - O que são dataclasses?
+# O módulo dataclasses fornece um decorador e funções para criar métodos como
+# __init__(), __repr__(), __eq__() (entre outros) em classes definidas pelo
+# usuário.
+# Em resumo: dataclasses são syntax sugar para criar classes normais.
+# Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
+# doc: https://docs.python.org/3/library/dataclasses.html
+
+from dataclasses import dataclass, field
+
+@dataclass
+class Pessoa:
+    nome: str
+    sobrenome: str
+    idade: int = 0
+    enderecos: list[str] = field(default_factory=list)
+
+    def __post_init__(self):
+        self.nome_completo = f'{self.nome} {self.sobrenome}'
+
+    # @property
+    # def nome_completo(self):
+    #     return f'{self.nome} {self.sobrenome}'
+
+    # @nome_completo.setter
+    # def nome_completo(self, nome_completo):
+    #     nome, *sobrenome = nome_completo.split()
+    #     self.nome = nome
+    #     self.sobrenome = ' '.join(sobrenome)
+
+if __name__ == '__main__':
+    p1 = Pessoa('Breno', 'Lima', 27)
+    # p1.nome_completo = 'Breno Santos Lima'
+    print(p1)
