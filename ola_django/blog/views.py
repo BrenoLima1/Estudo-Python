28d242d9fb697c140526d4fc600426878e34f6dc
Breno <brenolimanf@hotmail.com>
2025-10-30 20:21:23 -0300

    estudando Django

---
+++ ola_django/blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# from django.shortcuts import render
+
+
+def blog(request):
+    print('blog')
+    return render(request,
+                  'blog/index.html'
+    )
+
+
+def exemplo(request):
+    print('exemplo')
+    return render(
+        request,
+        'blog/exemplo.html')
